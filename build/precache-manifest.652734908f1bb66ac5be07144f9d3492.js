14629ed247487cddb35f54e938b1a91cf967c598
Bombu <rudy.anthoine@gmail.com>
2020-03-13 16:58:19 +0100

    ajoutdu lien dans readme

---
+++ build/precache-manifest.652734908f1bb66ac5be07144f9d3492.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf85893a1e5f0de8ef3dbc4f16668941",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a595b6434b46895b113",
+    "url": "/static/css/main.0cfe8232.chunk.css"
+  },
+  {
+    "revision": "78a91e5cd6e1fb5e2104",
+    "url": "/static/js/2.dd093476.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.dd093476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a595b6434b46895b113",
+    "url": "/static/js/main.21daccb1.chunk.js"
+  },
+  {
+    "revision": "ae4d0cd484e386043d40",
+    "url": "/static/js/runtime-main.c6040efa.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
